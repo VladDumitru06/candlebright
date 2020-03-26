1cb10d0a637374e641e8b175097db8a96028d8b4
Irina <I391975@fhict.nl>
2020-03-26 23:48:03 +0100

    Character controller/Movement/Scene setup

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    CharacterController Controller;
+    [SerializeField] private float playerSpeed = 10f;
+    private float movementSpeed;
+    private bool jump = false;
+    void Start()
+    {
+        Controller = GetComponent<CharacterController>();
+            
+    }
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A)) // Right
+        {
+            movementSpeed = playerSpeed * -1;
+            Debug.Log(movementSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D)) // Left
+        {
+            movementSpeed = playerSpeed;
+        }
+        else
+        {
+            movementSpeed = 0;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            jump = true;
+        }
+        else
+        {
+            jump = false;
+        }
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Controller.Move(movementSpeed, false, jump);
+    }
+}
