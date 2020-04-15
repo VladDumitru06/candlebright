8daa4e5281bfae0a0f3247e97cc7fa43ef17562d
Vlad <vlad.dumitru06@gmail.com>
2020-04-15 23:22:32 +0200

    Added two player, tweaked lights and movement a bit

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -11,8 +11,8 @@ public class CharacterController : MonoBehaviour
     [SerializeField] private Transform m_GroundCheck;                           // A position marking where to check if the player is grounded.
     [SerializeField] private Transform m_CeilingCheck;                          // A position marking where to check for ceilings
     [SerializeField] private Collider2D m_CrouchDisableCollider;                // A collider that will be disabled when crouching
-
-    const float k_GroundedRadius = .2f; // Radius of the overlap circle to determine if grounded
+    [SerializeField] public int playerNr;
+    const float k_GroundedRadius = .1f; // Radius of the overlap circle to determine if grounded
     private bool m_Grounded;            // Whether or not the player is grounded.
     const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
     private Rigidbody2D m_Rigidbody2D;
@@ -23,7 +23,7 @@ public class CharacterController : MonoBehaviour
     [Space]
 
     public UnityEvent OnLandEvent;
-
+    public int PlayerNr { get { return playerNr; } }
     [System.Serializable]
     public class BoolEvent : UnityEvent<bool> { }
 
@@ -61,75 +61,78 @@ public class CharacterController : MonoBehaviour
     }
 
 
-    public void Move(float move, bool crouch, bool jump)
+    public void Move(float move, bool crouch, bool jump, int PlayerNr)
     {
-        // If crouching, check to see if the character can stand up
-        if (!crouch)
+        if (PlayerNr == this.playerNr)
         {
-            // If the character has a ceiling preventing them from standing up, keep them crouching
-            if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
+            // If crouching, check to see if the character can stand up
+            if (!crouch)
             {
-                crouch = true;
-            }
-        }
-
-        //only control the player if grounded or airControl is turned on
-        if (m_Grounded || m_AirControl)
-        {
-
-            // If crouching
-            if (crouch)
-            {
-                if (!m_wasCrouching)
+                // If the character has a ceiling preventing them from standing up, keep them crouching
+                if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround))
                 {
-                    m_wasCrouching = true;
-                    OnCrouchEvent.Invoke(true);
-                }
-
-                // Reduce the speed by the crouchSpeed multiplier
-                move *= m_CrouchSpeed;
-
-                // Disable one of the colliders when crouching
-                if (m_CrouchDisableCollider != null)
-                    m_CrouchDisableCollider.enabled = false;
-            }
-            else
-            {
-                // Enable the collider when not crouching
-                if (m_CrouchDisableCollider != null)
-                    m_CrouchDisableCollider.enabled = true;
-
-                if (m_wasCrouching)
-                {
-                    m_wasCrouching = false;
-                    OnCrouchEvent.Invoke(false);
+                    crouch = true;
                 }
             }
 
-            // Move the character by finding the target velocity
-            Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-            // And then smoothing it out and applying it to the character
-            m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+            //only control the player if grounded or airControl is turned on
+            if (m_Grounded || m_AirControl)
+            {
 
-            // If the input is moving the player right and the player is facing left...
-            if (move > 0 && !m_FacingRight)
-            {
-                // ... flip the player.
-                Flip();
+                // If crouching
+                if (crouch)
+                {
+                    if (!m_wasCrouching)
+                    {
+                        m_wasCrouching = true;
+                        OnCrouchEvent.Invoke(true);
+                    }
+
+                    // Reduce the speed by the crouchSpeed multiplier
+                    move *= m_CrouchSpeed;
+
+                    // Disable one of the colliders when crouching
+                    if (m_CrouchDisableCollider != null)
+                        m_CrouchDisableCollider.enabled = false;
+                }
+                else
+                {
+                    // Enable the collider when not crouching
+                    if (m_CrouchDisableCollider != null)
+                        m_CrouchDisableCollider.enabled = true;
+
+                    if (m_wasCrouching)
+                    {
+                        m_wasCrouching = false;
+                        OnCrouchEvent.Invoke(false);
+                    }
+                }
+
+                // Move the character by finding the target velocity
+                Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+                // And then smoothing it out and applying it to the character
+                m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+                // If the input is moving the player right and the player is facing left...
+                if (move > 0 && !m_FacingRight)
+                {
+                    // ... flip the player.
+                    Flip();
+                }
+                // Otherwise if the input is moving the player left and the player is facing right...
+                else if (move < 0 && m_FacingRight)
+                {
+                    // ... flip the player.
+                    Flip();
+                }
             }
-            // Otherwise if the input is moving the player left and the player is facing right...
-            else if (move < 0 && m_FacingRight)
+            // If the player should jump...
+            if (m_Grounded && jump)
             {
-                // ... flip the player.
-                Flip();
+                // Add a vertical force to the player.
+                m_Grounded = false;
+                m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
             }
-        }
-        // If the player should jump...
-        if (m_Grounded && jump)
-        {
-            // Add a vertical force to the player.
-            m_Grounded = false;
-            m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
         }
     }
 
--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -7,6 +7,7 @@ public class CharacterMovement : MonoBehaviour
     CharacterController Controller;
     [SerializeField] private float playerSpeed = 10f;
     [SerializeField] private Canvas endtext;
+    [SerializeField] private int PlayerNr;
     private float movementSpeed;
     private bool jump = false;
     void Start()
@@ -15,28 +16,58 @@ public class CharacterMovement : MonoBehaviour
     }
     private void Update()
     {
-        if(Input.GetKey(KeyCode.A)) // Right
+        if (PlayerNr == 1)
         {
-            movementSpeed = playerSpeed * -1;
-            Debug.Log(movementSpeed);
-        }
-        else if (Input.GetKey(KeyCode.D)) // Left
-        {
-            movementSpeed = playerSpeed;
-        }
-        else
-        {
-            movementSpeed = 0;
-        }
+            if (Input.GetKey(KeyCode.A)) // Right
+            {
+                movementSpeed = playerSpeed * -1;
+                Debug.Log(movementSpeed);
+            }
+            else if (Input.GetKey(KeyCode.D)) // Left
+            {
+                movementSpeed = playerSpeed;
+            }
+            else
+            {
+                movementSpeed = 0;
+            }
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            jump = true;
+            if (Input.GetKey(KeyCode.W))
+            {
+                jump = true;
+            }
+            else
+            {
+                jump = false;
+            }
         }
-        else
-        {
-            jump = false;
+        else if(PlayerNr == 2)
+                {
+            if (Input.GetKey(KeyCode.LeftArrow)) // Right
+            {
+                movementSpeed = playerSpeed * -1;
+                Debug.Log(movementSpeed);
+            }
+            else if (Input.GetKey(KeyCode.RightArrow)) // Left
+            {
+                movementSpeed = playerSpeed;
+            }
+            else
+            {
+                movementSpeed = 0;
+            }
+
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                jump = true;
+            }
+            else
+            {
+                jump = false;
+            }
         }
+        
+       
     }
     // Update is called once per frame
     void FixedUpdate()
@@ -47,7 +78,7 @@ public class CharacterMovement : MonoBehaviour
         }
         else
         {
-            Controller.Move(movementSpeed, false, jump);
+            Controller.Move(movementSpeed, false, jump,PlayerNr);
         }
     }
 }
